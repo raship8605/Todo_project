4cb8d2600312fb13f5eed743a16714eb9553076a
rashi <rashipardeshi870@gmail.com>
2026-05-14 15:51:45 +0530

    Created Task Model

--- todo/models.py
+++ todo/models.py
@@ -2,3 +2,15 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class Task(models.Model):
+    title=models.CharField(max_length=200)
+    description=models.TextField(blank=True, null=True)
+    completed=models.BooleanField(default=False)
+    created_date=models.DateTimeField(auto_now_add=True)
+    completed_date=models.DateTimeField(blank=True,null=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering=['-created_date']
