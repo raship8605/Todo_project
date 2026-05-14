cea5f4acac7174cc1b9b5eafa09c91d13016889a
rashi <rashipardeshi870@gmail.com>
2026-05-14 16:46:52 +0530

    Created simple Todo application in Django

--- todo/views.py
+++ todo/views.py
@@ -1,25 +1,29 @@
-from django.shortcuts import render,redirect,get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 from django.utils import timezone
 
 # Create your views here.
 def task_list(request):
-    tasks=Task.objects.all()
-    return render(request, 'todo/task_list.html',{'tasks':tasks})
+    tasks = Task.objects.all()
+    return render(request, 'todo/task_list.html', {'tasks': tasks})
 
 def add_task(request):
-    if request=="POST":
-        title=request.POST.get('title')
-        description=request.POST.get('description')
+    if request.method == "POST":
+        title = request.POST.get('title')
+        description = request.POST.get('description')
         if title:
-            Task.objects.create(title=title,descritpion=description)
+            Task.objects.create(title=title, description=description)
         return redirect('task_list')
-    return render(request,'todo/add_task.html')
+    return render(request, 'todo/add_task.html')
 
-def complete_task(request,task_id):
-    task=get_object_or_404(Task,id=task_id)
-    task.completed=True
-    task.completed_date=timezone.now()
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.completed = True
+    task.completed_date = timezone.now()
     task.save()
     return redirect('task_list')
 
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('task_list')
